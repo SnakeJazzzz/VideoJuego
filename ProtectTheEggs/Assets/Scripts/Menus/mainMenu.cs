ab31afe5e957d918a020e930ab92155db7b4f8bd
Tomas Molina <a01784116@tec.mx>
2024-04-01 13:00:30 -0600

    Maps

--- ProtectTheEggs/Assets/Scripts/Menus/mainMenu.cs
+++ ProtectTheEggs/Assets/Scripts/Menus/mainMenu.cs
@@ -7,13 +7,11 @@ public class MainMenu : MonoBehaviour
     public void PlayGame()
     {
         
-        SceneManager.LoadScene(0); 
-        SceneManager.LoadSceneAsync(1, LoadSceneMode.Additive);
-        SceneManager.LoadSceneAsync(2, LoadSceneMode.Additive);
-        SceneManager.LoadSceneAsync(3, LoadSceneMode.Additive);
+        //SceneManager.LoadScene(0); 
+
         // Choose a random map index between 5 and 7 and load it additively
         int randomMapIndex = Random.Range(5, 8); // Random.Range is max-exclusive
-        SceneManager.LoadSceneAsync(randomMapIndex, LoadSceneMode.Additive);
+        SceneManager.LoadScene(randomMapIndex);
   
 
     }
