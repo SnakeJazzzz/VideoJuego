559ddaecbb337d49d7b99f14aa1b305fea013dab
SnakeJazzzz <michael2506@icloud.com>
2024-04-01 13:21:42 -0600

    interface

--- ProtectTheEggs/Assets/Scripts/Menus/mainMenu.cs
+++ ProtectTheEggs/Assets/Scripts/Menus/mainMenu.cs
@@ -6,12 +6,13 @@ public class MainMenu : MonoBehaviour
     // This function is called when the "Play" button is pressed.
     public void PlayGame()
     {
-        
-        //SceneManager.LoadScene(0); 
+       
+
 
         // Choose a random map index between 5 and 7 and load it additively
         int randomMapIndex = Random.Range(5, 8); // Random.Range is max-exclusive
         SceneManager.LoadScene(randomMapIndex);
+        
   
 
     }
