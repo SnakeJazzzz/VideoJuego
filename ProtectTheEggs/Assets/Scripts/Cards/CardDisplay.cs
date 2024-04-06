fd332aa8aee63c328dbf96d0f7ab7c694548c04a
SnakeJazzzz <michael2506@icloud.com>
2024-04-05 19:35:22 -0600

    Scene the creacion the mazos

--- ProtectTheEggs/Assets/Scripts/Cards/CardDisplay.cs
+++ ProtectTheEggs/Assets/Scripts/Cards/CardDisplay.cs
@@ -1,29 +1,57 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class CardDisplay : MonoBehaviour
 {
-    
-    Image image;
+    public Image artworkImage; // Drag your Image component that displays the card artwork here in the inspector.
+    public Card cardData; // The card data to display
+    public Button button; // Button component is not necessary if you are assigning the onClick in the Inspector.
 
-    void Awake()
+    private void Awake()
     {
-        image = GetComponent<Image>();
+        // If you need to get the Image component on the GameObject this script is attached to:
+        if (artworkImage == null)
+        {
+            artworkImage = GetComponent<Image>();
+        }
+
+
+        button.onClick.AddListener(() => FindObjectOfType<DeckBuilder>().AddCardToDeck(cardData));
+        
+    }
+
+    // Use this method to set up the card display with a specific card's data.
+    public void Initialize(Card card, bool isSelectable)
+    {
+        cardData = card;
+        artworkImage.sprite = card.artwork; // Assign the sprite to the artwork image.
+
+        button.onClick.RemoveAllListeners(); // Clear existing listeners
+        if (isSelectable)
+        {
+        // If the card is in the available pool, clicking it will add it to the selected deck
+        button.onClick.AddListener(() => FindObjectOfType<DeckBuilder>().AddCardToDeck(cardData));
+        }
+        else
+        {
+        // If the card is in the selected deck, clicking it will remove it from the selected deck
+        button.onClick.AddListener(() => FindObjectOfType<DeckBuilder>().RemoveCardFromDeck(cardData));
+        }
+}
+
+    // This method might be called elsewhere in your UI to update the card's display.
+    public void Display(Card card)
+    {
+        cardData = card;
+        artworkImage.sprite = card.artwork;
+        
+        // Additional display setup for card name, cost, etc., can go here if needed.
     }
 
     public void setActive(bool state)
     {
         gameObject.SetActive(state);
     }
-
-    public void Display(Card card)
-    {
-        
-        image.sprite = card.artwork;
-
-    }
-
     
+    // Add other methods as necessary that might be used by your UI.
 }
--- ProtectTheEggs/Assets/Scripts/Menus/mainMenu.cs
+++ ProtectTheEggs/Assets/Scripts/Menus/mainMenu.cs
@@ -10,10 +10,12 @@ public class MainMenu : MonoBehaviour
         // Choose a random map index between 5 and 7 and load it additively
         int randomMapIndex = Random.Range(5, 8); // Random.Range is max-exclusive
         SceneManager.LoadScene(randomMapIndex);
-        //SceneManager.LoadScene("Map2");
-     
+        //SceneManager.LoadScene("Map2"); 
+    }
 
-        
+    public void BuildDeck()
+    {
+        SceneManager.LoadScene("DeckBuilder");
     }
 
     public void QuitGame()
