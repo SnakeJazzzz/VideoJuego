03fdfcf9dea16de5aa93267bab7c44ffb6e6e707
a01029143 <a010291443@tec.mx>
2024-04-08 17:03:34 -0600

    Arreglo escala en GUI + cambios en documentación

---
+++ ProtectTheEggs/Assets/Scripts/Events/Log In/UsernameValidator.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System;
+
+public class UsernameValidator : MonoBehaviour
+{
+    public TMP_Text usernameText;
+    public TMP_Text passwordText;
+
+    public Action<string, string> ValidUsername;
+
+    public void Validate()
+    {
+        string username = usernameText.text;
+        string password = passwordText.text;
+        if (username.Length > 3 && password.Length > 3)
+        {
+            Debug.Log("Username: "+ username + "\nPassword: "+ password);
+            ValidUsername?.Invoke(username,password);
+        }
+        else
+        {
+            Debug.Log("El username y password tiene que ser mas de 3 o mas caracteres");
+        }
+    }
+
+}
--- ProtectTheEggs/Assets/Scripts/Log In/LogInCheck.cs
+++
@@ -1,9 +0,0 @@
-
-
-[System.Serializable]
-public class LogInCheck
-{
-    public bool Success;
-    public string Error;
-    // Add other fields expected from your JSON data.
-}
