904efd44676660e064f6f46c212aa8b3a91ed4d4
SnakeJazzzz <michael2506@icloud.com>
2024-04-22 22:40:49 -0600

    infinite waves map1,2 database towers

--- BaseDeDatos/bs_insert.sql
+++ BaseDeDatos/bs_insert.sql
@@ -8,27 +8,27 @@ INSERT INTO Usuarios (NombreUsuario, Contraseña, PuntuaciónMáxima) VALUES
 
 -- Insert data to NPC table
 INSERT INTO NPC (name, health, speed, attack, attackCooldown, `range`, isStructure, attackTowers, attackEnemies) VALUES 
-('Knight', 400, 1, 70, 1.5, 1, 0, 0, 1),
-('Archer', 150, 1, 40, 0.8, 10, 0, 0, 1),
-('Goblin', 50, 2, 35, 0.5, 1, 0, 0, 1),
-('Giant', 1000, 1, 450, 2, 1, 0, 0, 1),
-('Mage', 250, 1, 30, 1.5, 10, 0, 0, 1),
-('Ghost', 200, 1, 55, 0.8, 1, 0, 0, 1),
-('Orc', 350, 1.5, 65, 1, 1, 0, 0, 1),
-('Assassin', 250, 2, 150, 0.5, 1, 0, 0, 1),
-('Centaur', 450, 1, 100, 1.5, 2, 0, 0, 1),
-('Elf', 200, 1.5, 70, 1.5, 15, 0, 0, 1),
-('Berserker', 450, 1, 100, 0.8, 1, 0, 0, 1),
-('Ice Sorceress', 250, 1, 30, 1, 8, 0, 0, 1),
-('Stone Golem', 1250, 1, 150, 3, 1, 0, 0, 1),
-('Troll', 650, 1, 120, 2, 1, 0, 0, 1),
-('Scout', 200, 3, 50, 1, 2, 0, 0, 1),
-('Cannon', 500, 0, 80, 2, 2, 1, 0, 1),
-('Catapult', 400, 0, 120, 3, 3, 1, 0, 1),
-('Mortar Tower', 450, 0, 100, 2.5, 4, 1, 0, 1),
-('Archer Tower', 650, 0, 45, 1, 3, 1, 0, 1),
-('Inferno Tower', 550, 0, 20, 2, 2, 1, 0, 1),
-('Wizard Tower', 500, 0, 80, 1.5, 2, 1, 0, 1);
+('Knight', 400, 1, 70, 1.5, 1, 0, 1, 1),
+('Archer', 150, 1, 40, 0.8, 10, 0, 1, 1),
+('Goblin', 50, 2, 35, 0.5, 1, 0, 1, 1),
+('Giant', 1000, 1, 450, 1.5, 1, 0, 1, 1),
+('Mage', 250, 1, 30, 1.5, 10, 0, 1, 1),
+('Ghost', 200, 1, 55, 0.8, 1, 0, 1, 1),
+('Orc', 350, 1.5, 65, 1, 1, 0, 1, 1),
+('Assassin', 250, 2, 150, 0.5, 1, 0, 1, 1),
+('Centaur', 450, 1, 100, 1.5, 2, 0, 1, 1),
+('Elf', 200, 1.5, 70, 1.5, 15, 0, 1, 1),
+('Berserker', 450, 1, 100, 0.8, 1, 0, 1, 1),
+('Ice Sorceress', 250, 1, 30, 1, 8, 0, 1, 1),
+('Stone Golem', 1250, 1, 150, 3, 1, 0, 1, 1),
+('Troll', 650, 1, 120, 2, 1, 0, 1, 1),
+('Scout', 200, 3, 50, 1, 2, 0, 1, 1),
+('Cannon', 500, 0, 80, 1.5, 8, 1, 0, 1),
+('Catapult', 400, 0, 50, 3, 17, 1, 0, 1),
+('Mortar Tower', 450, 0, 100, 2, 10, 1, 0, 1),
+('Archer Tower', 650, 0, 45, 1, 8, 1, 0, 1),
+('Inferno Tower', 1500, 0, 800, 5, 6, 1, 0, 1),
+('Wizard Tower', 700, 0, 60, 1.5, 10, 1, 0, 1);
 /*
 ('Thorn Barrier', 800, 0, 30, 0, 0, 1, 0, 1),
 ('Bomber Tower', 600, 0, 100, 2.5, 2, 1, 0, 1);*/
@@ -50,12 +50,12 @@ INSERT INTO Cartas (cardName, description, cost, numberOfNPCs, IDNPC) VALUES
 ('Stone Golem', 'A living tank', 20, 1, 13),
 ('Troll', 'Strong and resilient', 7, 1, 14),
 ('Scout', 'Fast and agile', 6, 2, 15),
-('Cannon', 'Ideal for defense', 4, 1, 16),
-('Catapult', 'Great range', 5, 1, 17),
-('Mortar Tower', 'fortified positions', 6, 1, 18),
-('Archer Tower', 'Covering/ range', 5, 1, 19),
-('Inferno Tower', 'Ideal bigDamage', 6, 1, 20),
-('Wizard Tower', 'Casts spells', 7, 1, 21);
+('Cannon', 'Ideal for defense', 35, 1, 16),
+('Catapult', 'Great range', 40, 1, 17),
+('Mortar Tower', 'fortified positions', 35, 1, 18),
+('Archer Tower', 'Covering/ range', 35, 1, 19),
+('Inferno Tower', 'Ideal bigDamage', 40, 1, 20),
+('Wizard Tower', 'Casts spells', 40, 1, 21);
 
 -- Insert data to Mazos table for Usuarios
 INSERT INTO Mazos (IDUsuario, NombreMazo) VALUES 
