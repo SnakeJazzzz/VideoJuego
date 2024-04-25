eb835e956400bf607ee1bd28f7f89b434cbe2ef1
a01029143 <a010291443@tec.mx>
2024-04-25 15:17:48 -0600

    Mensaje en MapsMenu

--- ProtectTheEggs/Assets/Scripts/DeckBuild/GetAllCards.cs
+++ ProtectTheEggs/Assets/Scripts/DeckBuild/GetAllCards.cs
@@ -2,12 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
+using TMPro;
 
 public class GetAllCards : MonoBehaviour
 {
     public RSCards AvailableCards;
     public UserInformation userInformation;
     public string apiURL = "http://localhost:3000/api/card";
+    public GameObject textoMensajeErrorMenu;
+    public TMP_Text TextoErroresMenu;
 
     void Start()
     {
@@ -16,7 +19,6 @@ public class GetAllCards : MonoBehaviour
             AvailableCards.Items.Clear();
             StartCoroutine(APIGet());
         }
-       
     }
 
     IEnumerator APIGet()
@@ -28,6 +30,12 @@ public class GetAllCards : MonoBehaviour
         if(www.result != UnityWebRequest.Result.Success)
         {
             Debug.Log($"Request failed: {www.error}");
+
+            TextoErroresMenu.text = $"Request failed: {www.error}";
+            
+            textoMensajeErrorMenu.SetActive(true);
+            yield return new WaitForSeconds(2f);
+            textoMensajeErrorMenu.SetActive(false);
         }
         else 
         {
@@ -38,13 +46,9 @@ public class GetAllCards : MonoBehaviour
             CardList cartas = JsonUtility.FromJson<CardList>(data);
             for(int i = 0; i < cartas.Cartas.Count; i++)
             {
-                //
                 //Debug.Log(cartas.Cartas[i].cardName);
                 AvailableCards.Items.Add(new Card(cartas.Cartas[i]));
             }
-
-           
-            
         }
     }
 }
